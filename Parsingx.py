7f32a43ee0337b2f2d858bf1ebcf49ad8aee2355
Ismael <ismaelforest@gmail.com>
2022-04-08 23:26:51 -0400

    yes

--- Parsingx.py
+++ Parsingx.py
@@ -3,8 +3,7 @@ from pandas import ExcelWriter
 from pandas import ExcelFile
 
 
-expected = pd.read_excel('Book1.xlsx')
-output = pd.read_excel('Book10.xlsx')
+expected = pd.read_excel('Book1.xlsx')1
 column_heads = expected.columns
 first_row = " "
 
